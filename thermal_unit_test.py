51a7d7abda1e5772e0866b795cb0818cc49ec458
Gavin MacNeal <gkmacneal@wpi.edu>
2019-03-27 01:15:40 -0400

    added comments to help explain glumpy things

--- thermal_unit_test.py
+++ thermal_unit_test.py
@@ -5,9 +5,10 @@ import rospy
 from std_msgs.msg import Int16MultiArray
 import numpy as np
 from glumpy import app, gl, gloo
+# makes it so numpy arrays get fully printed
 np.set_printoptions(threshold=np.inf)
 
-# glumpy setup
+# glumpy shader setup
 vertex = """
          attribute vec2 position;
          void main()
@@ -21,41 +22,56 @@ fragment = """
                gl_FragColor = color;
            } """
 
-
-
+# array of raw ints coming from ROS
 data = np.zeros((4, 834), dtype=np.uint16)
+# to be used when converting to celsius
 celsius = np.zeros((4, 32, 24), dtype=np.float)
 
+# tracks which sections of which sensors have been read at least once
 read = np.zeros((4, 6), dtype=np.uint8)
 
+# glumpy window
 window = app.Window(1280, 240)
 
+# initialize a square for each pixel for each sensor
 quads = []
 for i in range(0, 128*24):
 	quad = gloo.Program(vertex, fragment, count=4)
 	quads.append(quad)
 
+# redraws each "pixel"
 @window.event
 def on_draw(dt):
 	window.clear()
 	for quad in quads:
 		quad.draw(gl.GL_TRIANGLE_STRIP)
 
+# ros subscriber callback
 def get_data(msg, args):
+    # which sensor sent the message
 	sensor = args
+    # where in the thermal array we are
 	offset = msg.layout.data_offset
+    # how many pixels we're getting at a time
 	size = msg.layout.dim[0].size
+    # copy data in from packet
 	data[sensor][offset:offset+size] = msg.data
+    # mark that we recieved a packet for this section of this sensor
 	read[sensor][int(offset/834)] = 1
-	temp_pixels = np.reshape(data[:,:768], (128, 24))
-	for i in range(32*sensor, 32*sensor+32):
+    # puts the values from raw data into a format for glumpy
+    temp_pixels = np.reshape(data[:,:768], (128, 24))
+    # update all the glumpy pixel colors we recieved
+    for i in range(32*sensor, 32*sensor+32):
 		for j in range(0, 24):
+            # position is constant, move this outside get_data() to improve performance
 			quads[i*24+j]["position"] = [((float(i-64) / 68.0)-0.05, (float(j-12) / -13.0)-0.05),
 								((float(i-64) / 68.0)+0.05, (float(j-12) / -13.0)-0.05),
 								((float(i-64) / 68.0)-0.05, (float(j-12) / -13.0)+0.05),
 								((float(i-64) / 68.0)+0.05, (float(j-12) / -13.0)+0.05)]
-			quads[i*24+j]["color"] = float(temp_pixels[i,j]) / 65535.0
+            # color of each pixel
+            quads[i*24+j]["color"] = float(temp_pixels[i,j]) / 65535.0
 
+    # convert to celsius
 	'''
 	if np.sum(read[sensor]) == 6:
 		to_celsius(sensor)
@@ -126,12 +142,15 @@ def to_celsius(sensor):
 
 if __name__ == "__main__":
 	try:
+        # init node
 		rospy.init_node('thermal_unit_test')
+        # init subscribers
 		rospy.Subscriber('/thermal_0i',  Int16MultiArray, get_data, callback_args=(0), queue_size=10)
 		rospy.Subscriber('/thermal_1i',  Int16MultiArray, get_data, callback_args=(1), queue_size=10)
 		rospy.Subscriber('/thermal_2i',  Int16MultiArray, get_data, callback_args=(2), queue_size=10)
 		rospy.Subscriber('/thermal_3i',  Int16MultiArray, get_data, callback_args=(3), queue_size=10)
 		rospy.sleep(1)
+        # init glumpy app
 		app.run()
 		while(not rospy.is_shutdown()):
 			pass
