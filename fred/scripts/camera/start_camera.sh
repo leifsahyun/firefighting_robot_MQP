4752cb263005baead035547ff2d337b5cdf856e5
Leif Sahyun <lgsahyun@wpi.edu>
2020-03-30 18:53:39 -0400

    Modified (Unknown) start_camera.sh , uploading sensor data samples

--- fred/scripts/camera/start_camera.sh
+++ fred/scripts/camera/start_camera.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-raspivid -t 0 -fps 30 -o - | nc -u -l -k 1234
+DEST_HOSTNAME=autoreg-071220.dyn.wpi.edu
+PORT=3333
+raspivid --vstab -w 640 -h 480 -t 0 -fps 30 -o - | nc -u $DEST_HOSTNAME $PORT
