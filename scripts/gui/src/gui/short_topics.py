da9846718fa00f49fbfb2854e57b922d96e3b399
leifsahyun <leif@sahyun.com>
2020-04-15 11:49:13 -0400

    Changed some topic names in short_topics to match sensor output

--- scripts/gui/src/gui/short_topics.py
+++ scripts/gui/src/gui/short_topics.py
@@ -8,13 +8,14 @@ class ShortTopic(Topic):
 	def __init__(self, context):
 		super(ShortTopic, self).__init__(context)
 		self._widget.set_selected_topics([
-			("/extern_temp_sensor","sensor_msgs/Temperature"),
-			("/extern_temp_sensor/temperature","std_msgs/Float64"),
-			("/intern_temp_sensor","sensor_msgs/Temperature"),
-			("/intern_temp_sensor/temperature","std_msgs/Float64"),
-			("/BME680-pressure","sensor_msgs/FluidPressure"),
-			("/BME680-pressure/fluid_pressure","std_msgs/Float64"),
+			("/extern_temp","sensor_msgs/Temperature"),
+			("/extern_temp/temperature","std_msgs/Float64"),
+			("/intern_temp","sensor_msgs/Temperature"),
+			("/intern_temp/temperature","std_msgs/Float64"),
+			("/intern_pressure","sensor_msgs/FluidPressure"),
+			("/intern_pressure/fluid_pressure","std_msgs/Float32"),
 			("/battery","sensor_msgs/BatteryState"),
-			("/battery/voltage","std_msgs/String")
+			("/battery/voltage","std_msgs/String"),
+			("/radar","sensor_msgs/Range")
 		])
 
